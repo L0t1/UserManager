5de5c444c98c8a12452abe65c55837a3a61330f2
Lot Kyçyku <lotkyqyku@gmail.com>
2023-06-12 14:37:08 +0200

    Added guest/default layout and dashboard

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,23 +1,46 @@
-import {createBrowserRouter} from 'react-router-dom';
+import {Navigate, createBrowserRouter} from 'react-router-dom';
 import NotFound from './views/NotFound';
 import Login from './views/Login';
 import Signup from './views/Signup';
 import Users from './views/Users';
+import Dashboard from './views/Dashboard';
 
+import DefaultLayout from './components/DefaultLayout';
+import GuestLayout from './components/GuestLayout';
 
 
 const router = createBrowserRouter([
     {
-        path: '/login',
-        element: <Login />
+        path: '/',
+        element: <DefaultLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Navigate to={"/users"} />
+            },
+            {
+                path: '/users',
+                element: <Users />
+            },
+            {
+                path: '/dashboard',
+                element: <Dashboard />
+            },
+        ]
     },
     {
-        path: '/signup',
-        element: <Signup />
-    },
-    {
-        path: '/users',
-        element: <Users />
+        path: '/',
+        element: <GuestLayout />,
+        children: [
+            {
+                path: '/login',
+                element: <Login />
+            },
+            {
+                path: '/signup',
+                element: <Signup />
+            },
+        ]
     },
     {
         path: '*',
